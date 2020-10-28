c5cae78fd47c6342276452cacf06e3b56a6cbab4
Hongyi Dan <briandhy@hotmail.com>
2020-10-28 16:42:56 -0400

    looper

--- looper/isloop.js
+++ looper/isloop.js
@@ -1,10 +1,34 @@
-'use strict';
+"use strict";
+
+const LinkedList = require("./linkedlist");
 
 //Complete this algo
+//traverse the linked list, memorize each node,if we encounter a node with
+//repeatable value, return true
+//otherwise, return false
+
+/* const isLoop = (linkedlist) => {
+  let currentNode = linkedlist.head;
+  const memory = {};
+  while (currentNode) {
+    if (currentNode.value in memory) return true;
+    else memory[currentNode.value] = 1;
+    currentNode = currentNode.next;
+  }
+  return false;
+}; */
+
 const isLoop = (linkedlist) => {
-
+  let currentNode = linkedlist.head;
+  const memory = {};
+  while (currentNode) {
+    if (currentNode.value in memory) return true;
+    else memory[currentNode.value] = 1;
+    //console.log(memory);
+    currentNode = currentNode.next;
+  }
+  return false;
 };
-
 
 /*
 EXTRA CREDIT:
@@ -14,4 +38,18 @@ This function should return the Node value the loop begins at
 Remember to write some test specs too!
 
 */
-module.exports = isLoop
+
+const findLoop = (linkedlist) => {
+  let currentNode = linkedlist.head;
+  const memory = {};
+  while (currentNode) {
+    if (currentNode.value in memory) {
+      console.log(currentNode.value);
+      return currentNode.value;
+    } else memory[currentNode.value] = 1;
+    currentNode = currentNode.next;
+  }
+  return null;
+};
+
+module.exports = { isLoop, findLoop };
